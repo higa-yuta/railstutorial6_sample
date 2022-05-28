628e994f261cc25e3cd54f07ae8026e41b5cebc0
yuta higa <h.yuta2314@gmail.com>
2022-05-28 06:20:38 +0000

    Change database references; Modified database.yml.

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -2,12 +2,17 @@ require "test_helper"
 
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
   test "should get home" do
-    get static_pages_home_url
+    get home_url
     assert_response :success
   end
 
   test "should get help" do
-    get static_pages_help_url
+    get help_url
+    assert_response :success
+  end
+
+  test "should get about" do
+    get about_url
     assert_response :success
   end
 end
