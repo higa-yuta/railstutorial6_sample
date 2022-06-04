570e3f3781242392b3cf293017d0c83345c2ec43
yuta higa <h.yuta2314@gmail.com>
2022-06-04 19:46:35 +0000

    User_login integration test generated

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,8 +4,20 @@ class SessionsController < ApplicationController
   end
 
   def create
+    user = User.find_by_email(session_params[:email].downcase)
+    if user&.authenticate(session_params[:password])
+
+    else
+      flash.now[:danger] = "メードアドレスまたはパスワードが異なります"
+      render :new
+    end
   end
 
   def destroy
   end
+
+  private
+    def session_params
+      params.require(:session).permit(:email, :password)
+    end
 end
