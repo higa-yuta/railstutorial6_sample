3f3cb58d2694a9e826eb87b760b8d86471d36c93
yuta higa <h.yuta2314@gmail.com>
2022-07-13 10:47:20 +0000

    Generate activation_digest cloumns and it was migrate

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,7 +3,9 @@ User.create!(name: "okinawa taro",
              email: 'okinawa@sample.com',
              password: 'password',
              password_confirmation: 'password',
-             admin: true)
+             admin: true,
+             activated: true,
+             activated_at: Time.zone.now)
 
 99.times do |i|
   name = Faker::Name.name
@@ -12,5 +14,7 @@ User.create!(name: "okinawa taro",
   User.create!(name: name,
                email: email,
                password: password,
-               password_confirmation: password)
+               password_confirmation: password,
+               activated: true,
+               activated_at: Time.zone.now)
 end
